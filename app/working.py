18110546951822bb4ea0cb6292e3b709d121af95
daniil <daniilviking@gmail.com>
2023-06-12 11:10:03 +0300

    optimize get_prices algorythm

--- app/working.py
+++ app/working.py
@@ -1,20 +1,24 @@
 import time
+
 from appium.webdriver.common.appiumby import AppiumBy
 import datetime
 from appium import webdriver
 from selenium.common import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 capabilities = dict(
     platformName="Android",
     automationName="uiautomator2",
-    deviceName="Android",
+    deviceName="9c6ceb8e",
     appPackage="com.tripadvisor.tripadvisor",
     appActivity="com.tripadvisor.android.ui.primarynavcontainer.MainActivity",
+    appWaitActivity="*",
     language="en",
     locale="US",
 )
 
-appium_server_url = "http://localhost:4723"
+appium_server_url = "http://localhost:4723/wd/hub"
 
 
 class MyTest:
@@ -25,14 +29,13 @@ class MyTest:
         size = self.driver.get_window_size()
 
         if not up:
-            start_x = size["width"] // 2
             start_y = int(size["height"] * 0.6)
             end_y = int(size["height"] * 0.4)
         else:
-            start_x = size["width"] // 2
             start_y = int(size["height"] * 0.3)
             end_y = int(size["height"] * 0.1)
 
+        start_x = size["width"] // 2
         self.driver.swipe(start_x=start_x, start_y=start_y, end_x=start_x, end_y=end_y)
 
     def select_one(self, text, contains=False):
@@ -55,6 +58,11 @@ class MyTest:
                 by=AppiumBy.XPATH, value=self.full_match.format(text)
             )
 
+    def click_on_btn(self, btn_name):
+        button = self.select_one(btn_name)
+        self.wait.until(EC.element_to_be_clickable(button))
+        self.make_click(button)
+
     @staticmethod
     def make_click(el):
         el.click()
@@ -62,26 +70,23 @@ class MyTest:
     def full_calendar_up(self):
         self.driver.find_element(
             AppiumBy.ANDROID_UIAUTOMATOR,
-            f"new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollToBeginning(8);",
+            "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollToBeginning(8);",
         )
-        time.sleep(1)
 
     def select_date(self, desired_date):
         date_object = datetime.datetime.strptime(desired_date, "%m-%d-%Y")
         day = date_object.day
         year = date_object.year
         month = date_object.strftime("%B")
-        month_year = str(month) + " " + str(year)
+        month_year = f"{str(month)} {str(year)}"
 
         calendar_button = self.driver.find_element(
             by=AppiumBy.ID, value="com.tripadvisor.tripadvisor:id/txtDate"
         )
-
+        self.wait.until(EC.element_to_be_clickable(calendar_button))
         calendar_button.click()
-        time.sleep(3)
 
         self.full_calendar_up()
-        time.sleep(3)
 
         for _ in range(100):
             try:
@@ -92,20 +97,14 @@ class MyTest:
                 screen_size = self.driver.get_window_size()
                 screen_height = screen_size["height"]
 
-                if abs(desired_field_location["y"] - screen_height // 2) < 100:
-                    break
-                else:
+                if abs(desired_field_location["y"] - screen_height // 2) >= 100:
                     self.make_swipe()
-                    break
+                break
             except:
                 self.make_swipe()
 
-        date_button = self.select_one(day)
-        self.make_click(date_button)
-
-        apply_button = self.select_one("Apply")
-        self.make_click(apply_button)
-        time.sleep(10)
+        self.click_on_btn(day)
+        self.click_on_btn("Apply")
 
     def get_provider_price_data(self):
         info_dict = {}
@@ -136,7 +135,6 @@ class MyTest:
                     price = None
                 if price and provider:
                     info_dict[provider] = price
-                time.sleep(1)
             self.make_swipe()
 
         return info_dict
@@ -149,10 +147,10 @@ class MyTest:
                 break
             except:
                 self.make_swipe()
-                time.sleep(1)
 
     def setUp(self) -> None:
         self.driver = webdriver.Remote(appium_server_url, capabilities)
+        self.wait = WebDriverWait(self.driver, 10)
         time.sleep(3)
 
     def tearDown(self) -> None:
@@ -161,50 +159,45 @@ class MyTest:
 
     def get_prices(self, hotel_data) -> dict:
         self.setUp()
-        dict_data = {}
+        dict_data = {}.items()
 
         for hotel_name, dates_list in hotel_data.items():
-            try:
-                dict_data[hotel_name] = {}
-                search_button = self.select_one("Search")
-                self.make_click(search_button)
 
-                time.sleep(3)
+            # dict_data[hotel_name] = {}
+            self.click_on_btn("Search")
+            self.click_on_btn("Where to?")
+            self.click_on_btn("Where to?")
 
-                where_to = self.select_one("Where to?")
-                self.make_click(where_to)
-                time.sleep(3)
+            where_to = self.select_one("Where to?")
+            where_to.send_keys(hotel_name)
 
-                where_to = self.select_one("Where to?")
-                where_to.send_keys(hotel_name)
-                self.driver.keyevent(66)
-                time.sleep(5)
+            self.driver.keyevent(66)
+            self.wait.until_not(EC.element_to_be_clickable(where_to))
 
-                self.select_hotel(hotel_name)
-                time.sleep(3)
+            self.select_hotel(hotel_name)
 
-                for date_in_hotel in dates_list:
-                    try:
-                        self.select_date(date_in_hotel)
+            for date_in_hotel in dates_list:
+                try:
+                    self.select_date(date_in_hotel)
 
-                        view_all_button = self.select_one("View all", True)
-                        self.make_click(view_all_button)
-                        time.sleep(10)
+                    view_all_button = self.select_one("View all", True)
+                    self.make_click(view_all_button)
+                    self.wait.until_not(EC.element_to_be_clickable(view_all_button))
 
-                        dict_data[hotel_name][
-                            date_in_hotel
-                        ] = self.get_provider_price_data()
-                        self.driver.back()
-                    except NoSuchElementException | NoSuchElementException as e:
-                        print(
-                            f"Exception occured while processing data at hotel :{hotel_name}, date: {date_in_hotel} exception: {e}"
-                        )
-                        continue
-            except Exception as e:
-                print(
-                    f"Exception occured while processing data at hotel :{hotel_name}, exception: {e}"
-                )
+                    dict_data[hotel_name][
+                        date_in_hotel
+                    ] = self.get_provider_price_data()
+                    self.driver.back()
+                except NoSuchElementException | NoSuchElementException as e:
+                    print(
+                        f"Exception occured while processing data at hotel :{hotel_name}, date: {date_in_hotel} exception: {e}"
+                    )
 
         self.tearDown()
 
         return dict_data
+
+
+# if __name__ == "__main__":
+#     a = MyTest()
+#     a.get_prices({"New York Hilton Midtown": ["06-10-2023", "06-12-2023"]})
